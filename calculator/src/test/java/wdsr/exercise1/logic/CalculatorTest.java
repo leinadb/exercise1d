96a8c2e49b17854dd4b8f57d9eba3f24889aa53e
slabykonrad <konrad.slaby@wp.pl>
2017-03-19 13:31:09 +0100

    added new tests and fixed max method

--- calculator/src/test/java/wdsr/exercise1/logic/CalculatorTest.java
+++ calculator/src/test/java/wdsr/exercise1/logic/CalculatorTest.java
@@ -1,6 +1,8 @@
 package wdsr.exercise1.logic;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -59,5 +61,77 @@ public class CalculatorTest {
 		
 		// then
 		// empty
-	}		
+	}	
+
+	@Test
+	public void testMax_shouldReturnTheOnlyValue() {
+		// given
+		int[] values = { 5 };
+		
+		// when
+		int max = calculator.max(values);
+		
+		// then
+		assertThat(values[0], is(equalTo(max)));
+	}
+
+	@Test
+	public void testMax_shouldReturnTheLargestOfAllValues() {
+		// given
+		int[] values = { 0, -1, 6, -3, 5 };
+		
+		// when
+		int max = calculator.max(values);
+		
+		// then
+		assertThat(6, is(equalTo(max)));
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void testMax_shouldRaiseAnExceptionForNullArgument() {
+		// given
+		int[] values = null;
+		
+		// when
+		calculator.max(values);
+		
+		// then
+		// empty
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void testMax_shouldRaiseAnExceptionForEmptyArgument() {
+		// given
+		int[] values = {};
+		
+		// when
+		calculator.max(values);
+		
+		// then
+		// empty
+	}
+
+	@Test
+	public void testMax_shouldReturnTheOneOfAllNegativeValues() {
+		// given
+		int[] values = { -1, -1, -1, -1, -1 };
+		
+		// when
+		int max = calculator.max(values);
+		
+		// then
+		assertThat(-1, is(equalTo(max)));
+	}
+
+	@Test
+	public void testMax_shouldReturnTheLargestOfAllNegativeValues() {
+		// given
+		int[] values = { -10, -3, -2, -1, -6 };
+		
+		// when
+		int max = calculator.max(values);
+		
+		// then
+		assertThat(-1, is(equalTo(max)));
+	}
 }
